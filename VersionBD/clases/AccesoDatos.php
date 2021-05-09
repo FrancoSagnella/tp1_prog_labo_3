f51030cd2ea219c7c456d6a7dddc418508ea2066
Franco Sagnella <fransagn@gmail.com>
2021-05-09 01:06:54 -0300

    asdadddwwsa

--- VersionBD/clases/AccesoDatos.php
+++ VersionBD/clases/AccesoDatos.php
@@ -7,7 +7,7 @@ class AccesoDatos
     private function __construct()
     {
         try {
-
+            //$this->_objPDO  = new PDO('mysql:host=localhost;dbname=tp_bd;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $this->_objPDO  = new PDO('mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_a33f45fa011b954;charset=utf8', 'b1a81ab4c3eefd', '3b3806c6', array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             //b1a81ab4c3eefd:3b3806c6@us-cdbr-east-03.cleardb.com/heroku_a33f45fa011b954?reconnect=true
         } catch (PDOException $e) {
